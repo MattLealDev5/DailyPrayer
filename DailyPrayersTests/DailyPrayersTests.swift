be4c107aacaded953857ef142f3a3a8072fabe7a
FishEggsThe <matthewfleal@gmail.com>
2025-10-12 13:19:06 -0400

    Initial Commit

---
+++ DailyPrayersTests/DailyPrayersTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyPrayersTests.swift
+//  DailyPrayersTests
+//
+//  Created by Matthew Leal on 10/12/25.
+//
+
+import Testing
+@testable import DailyPrayers
+
+struct DailyPrayersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
